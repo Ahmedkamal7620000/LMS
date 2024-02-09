974b1f7c005e23f98bc3a74c2aa096b2c84cf492
Ahmedkamal7620000 <ahmedkamal7620000@gmail.com>
2024-02-09 23:38:39 +0200

    solve error

--- app/user_login.py
+++ app/user_login.py
@@ -1,6 +1,14 @@
 from django.shortcuts import redirect, render
 
+
 def REGISTER(request):
-    print(request.method)
-    print(request.POST)
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(username,email,password) 
+        print(request.method)
+        print(request.POST)
     return render(request, 'registration/register.html')
+    
